0e24714be40dd03c78e3224da08d1ed03e864395
Luca.Pellizzari <lucapellizzari@libero.it>
2018-06-08 10:39:46 +0200

    Added firsr project

---
+++ GitTest/GitTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Git! This Is my first repository!");
+            Console.Read();
+        }
+    }
+}
