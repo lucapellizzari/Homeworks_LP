d5dd190aec7dd1c14d9a45881611e3b49cd9b43c
Luca.Pellizzari <lucapellizzari@libero.it>
2018-06-08 11:06:29 +0200

    Added GitTest2_new_repo project

---
+++ GitTest2_new_repo/GitTest2_new_repo/ProgramA.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GitTest2_new_repo
+{
+    class ProgramA
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Git Test 2!");
+            Console.Read();
+        }
+    }
+}
